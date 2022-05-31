630fe6800771d4a07403db476d85710fbfd2050d
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-05-31 07:07:29 +0300

    Update 09.login.js

--- Fundamentals Module/Basic Syntax, Conditional Statements and Loops/Exercise - Basic Syntax, Conditional Statements and Loops/09.login.js
+++ Fundamentals Module/Basic Syntax, Conditional Statements and Loops/Exercise - Basic Syntax, Conditional Statements and Loops/09.login.js
@@ -1,4 +1,32 @@
-function login() {
+function login(input) {
+    let index = 0;
+    let username = input[index];
+    index++;
+    let password = '';
+    let wrongPassCount = 0;
+    let logingTries = input[index];
+    index++;
+     for (let i = username.length - 1; i >= 0; i--) {
+         password += username[i];
+     }
+
+     while (password !== logingTries) {
+         wrongPassCount++;
+        logingTries = input[index];
+        index++;
+        if ( wrongPassCount === 4) {
+            console.log(`User ${username} blocked!`);
+            break;
+        } else {
+            console.log("Incorrect password. Try again.");
+        }
+     }
+     
+     if (password === logingTries) {
+         console.log(`User ${username} logged in.`);
+     }
      
 }
-login()
+//login(['Acer','login','go','let me in','recA'])
+//login(['momo','omom'])
+login(['sunny','rainy','cloudy','sunny','not sunny'])
