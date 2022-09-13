ec0096b41cfe56e836775f5f502562b5d58a4408
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-13 21:50:02 +0300

    Need to check again this problem

---
+++ Advanced JS Module/JS Advanced/Syntax, Functions and Statements/Exercise/08.validityChecker.js
@@ -0,0 +1,25 @@
+function validityChecker(x1, y1, x2, y2) {
+    let distance;
+    distance = Math.sqrt(((x1 - 0)**2) + ((y1 - 0)**2));
+        if(distance.isInteger()) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+        } else {
+        console.log(`${x1}, ${y1}} to {0, 0} is invalid`);
+        }
+
+    distance = Math.sqrt(((x2 - 0)**2) + ((y2 - 0)**2));
+        if(distance.isInteger()) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+        } else {
+        console.log(`${x1}, ${y1}} to {0, 0} is invalid`);
+        }
+
+    distance = Math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2));
+    if(distance.isInteger()) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+
+}
+validityChecker(3, 0, 0, 4)
