e29d6f63bb91c2681efa756eade930159b3e4ab6
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-14 19:57:16 +0300

    I couldn't figure out why this is the solution.

--- Advanced JS Module/JS Advanced/Syntax, Functions and Statements/Exercise/02.greatestCommonDivisor.js
+++ Advanced JS Module/JS Advanced/Syntax, Functions and Statements/Exercise/02.greatestCommonDivisor.js
@@ -1,4 +1,15 @@
-function gCD(num1, num2) {
- 
-}
-gCD(15, 5)
+function gcd(a,b) {
+        if (b > a) {
+            let temp = a; 
+            a = b; 
+            b = temp;
+        }
+
+        while (true) {
+            if (b == 0) return a;
+            a %= b;
+            if (a == 0) return b;
+            b %= a;
+        }
+    }
+console.log(gcd(2154, 458))
