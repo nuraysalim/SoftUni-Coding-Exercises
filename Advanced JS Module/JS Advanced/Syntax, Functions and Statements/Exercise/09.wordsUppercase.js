c896dae0dbe2b6caa4860ee2edc54dca4cc21fda
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-14 17:20:37 +0300

    Optimized my original solution(it was much needed); Also added instructions for solving it in a different way

--- Advanced JS Module/JS Advanced/Syntax, Functions and Statements/Exercise/09.wordsUppercase.js
+++ Advanced JS Module/JS Advanced/Syntax, Functions and Statements/Exercise/09.wordsUppercase.js
@@ -1,10 +1,9 @@
 function wordsUppercase(str) {
-    const pattern = /\w+/gm;
-    const res = [];
-    const matched = str.match(pattern).forEach(word => {
-        res.push(word.toUpperCase())
-    });;;
-   
-    console.log(res.join(', ')); 
+  return str.match(/\w+/g).join(', ').toUpperCase();
 }
-wordsUppercase('Hi, how are you?')
+console.log(wordsUppercase('Hi, how are you?'))
+console.log(wordsUppercase('hello'));
+/* Another way of solving the problem:
+- Using .charCodeAt() compare if the characters' ASCII code is in the range for only letters.
+! You can make all the letter of the str .toLower() case so you have less if cases
+*/ 
