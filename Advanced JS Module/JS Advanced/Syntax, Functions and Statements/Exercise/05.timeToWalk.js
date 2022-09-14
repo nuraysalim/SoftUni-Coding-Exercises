15be94709310a4dd55390b47897c7ed654640999
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-14 12:27:16 +0300

    Added instructions on how to solve problem 5 that I experienced difficulty solving

--- Advanced JS Module/JS Advanced/Syntax, Functions and Statements/Exercise/05.timeToWalk.js
+++ Advanced JS Module/JS Advanced/Syntax, Functions and Statements/Exercise/05.timeToWalk.js
@@ -1,4 +1,11 @@
 function timeToWalk(steps, footprint, speed) {
-    
+    /*\How to solve the problem instructions:
+    - We find the distance in meters by multiplying the steps with the footprint;
+    - Transform the speed into m/s by diving the speed by 3.6;
+    - Then find the time;
+    - You should find the time in minutes, then in seconds and in hours;
+    - Pay attention what happens when minutes are more than 60;
+    - And format the results in such a way to be double digits;  
+    */
 }
 timeToWalk(4000, 0.60, 5)
