21cb26cde1fafd3fb4356be565d5be51cc7541e7
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-15 18:01:29 +0300

    Nothing changed - just formatting

--- Advanced JS Module/JS Advanced/1. Syntax, Functions and Statements/Exercise/04.previousDay.js
+++ Advanced JS Module/JS Advanced/1. Syntax, Functions and Statements/Exercise/04.previousDay.js
@@ -1,4 +1,3 @@
-/*
 function previousDay(year, month, day) {
     const aDayBefore = new Date(year, month-1, day-1);
     
@@ -6,14 +5,15 @@ function previousDay(year, month, day) {
 }
 previousDay(2016, 9, 30)
 previousDay(2016, 10, 1)
-*/
+
 
 /* Another way of solving the problem: 
 -Use a format -> date as string(interpolation 14/9/2022) ** Apparently you cannot use .setDate()
 on a new Date(`${year}, ${month}, ${day}`), also if you give a value of 0 to day - new Date()
 returns NaN.
 -Use .setDate() to get the date one day before 
-*/
+
+
 function previousDay(year, month, day) {
     let date = new Date(year, month - 1, day);
     date.setDate(day - 1);
@@ -22,3 +22,4 @@ function previousDay(year, month, day) {
 previousDay(2016, 9, 30)
 console.log('------------------');
 previousDay(2016, 10, 1)
+*/
