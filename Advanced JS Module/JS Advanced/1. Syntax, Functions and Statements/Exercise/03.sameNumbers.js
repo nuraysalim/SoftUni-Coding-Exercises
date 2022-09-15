26fe4e8d36375c849c5b3bae6c240344880a6a7f
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-15 17:11:41 +0300

    Another solution of problem 3 from Exercise - Syntax, Functions and Statements
    Optimazed my solution and corrected my misused of .forEach()

--- Advanced JS Module/JS Advanced/1. Syntax, Functions and Statements/Exercise/03.sameNumbers.js
+++ Advanced JS Module/JS Advanced/1. Syntax, Functions and Statements/Exercise/03.sameNumbers.js
@@ -1,8 +1,38 @@
+//Optimazed solution:
+/*- Transform num to String;
+  - Use normal for loop
+*/
+
+function sameNumbers(num) {
+  let numToStr = num.toString();
+  let firstDigit = numToStr[0];
+  let isSame = true;
+  let sumOfDigits = Number(firstDigit);
+
+  for (let i = 1; i < numToStr.length; i++) {
+    if (firstDigit !== numToStr[i]) {
+      isSame = false;
+    }
+    
+    sumOfDigits += Number(numToStr[i]);
+  }
+
+    console.log(isSame);
+    console.log(sumOfDigits);
+}
+sameNumbers(222222222222)
+console.log('----------------')
+sameNumbers(1234)
+
+/*First solution:
+
 function sameNums(num) {
     let sum = 0;
     let previousDigit = num.toString().split('')[0];
     let areSame;
     
+    // I'm not using the .forEach() here correctly. When you use it the variable
+    //that you use (here - res) should be assigned a value and you should use the value.
  let res = num.toString().split('').forEach(digit => {
    sum += Number(digit);
 
@@ -18,8 +48,5 @@ function sameNums(num) {
  console.log(sum);
 }
 sameNums(222222223)
+*/ 
 
-//Another solution:
-/*- Transform num to String;
-  - Use normal for loop
-*/
