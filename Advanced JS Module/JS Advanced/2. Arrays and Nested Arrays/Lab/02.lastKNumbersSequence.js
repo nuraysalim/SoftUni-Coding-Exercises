d64ffd0cc91967794c25134111b30b3e1d00dfc2
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-15 16:51:21 +0300

    I solved problem 2 from Lab - Arrays and Nested Arrays

--- Advanced JS Module/JS Advanced/2. Arrays and Nested Arrays/Lab/02.lastKNumbersSequence.js
+++ Advanced JS Module/JS Advanced/2. Arrays and Nested Arrays/Lab/02.lastKNumbersSequence.js
@@ -1,10 +1,19 @@
 function lastNumSeq(n, k) {
     let res = [1];
 
-    while (res.length <= n) {
-        for(let i = res.length; i > 0; i--) {
-
-        }
+    while (res.length < n) {
+            let lastKEls = res.slice(-k);
+            
+            if(lastKEls.length === 1) {
+                res.push(1);
+            } else {
+                let sumOfEls = lastKEls.reduce((acc, curr) => acc += curr);
+                res.push(sumOfEls); 
+            }
     }
+
+    return res;
 }
-lastNumSeq(6, 3)
+console.log(lastNumSeq(6, 3));
+console.log('--------------');
+console.log(lastNumSeq(8, 2));
