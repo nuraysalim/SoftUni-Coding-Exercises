ad9630ccbcb8a5207a984f3f33df7054dece55f2
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-25 21:24:45 +0300

    05. Extract Increasing Subsequence from Array solved with .reduce()

--- Advanced JS Module/JS Advanced/2. Arrays and Nested Arrays/Exercise/05.extractIncreasingSubsequenceFromArray.js
+++ Advanced JS Module/JS Advanced/2. Arrays and Nested Arrays/Exercise/05.extractIncreasingSubsequenceFromArray.js
@@ -1,19 +1,26 @@
 function extracting(arr) {
-    let res = [arr[0]];
-    let currBiggest = arr[0];
+    // let res = [arr[0]];
+    // let currBiggest = arr[0];
 
-    for (let i = 1; i < arr.length; i++) {
-        if(currBiggest <= arr[i]) {
-            currBiggest = arr[i];
-            res.push((arr[i]));
+    // for (let i = 1; i < arr.length; i++) {
+    //     if(currBiggest <= arr[i]) {
+    //         currBiggest = arr[i];
+    //         res.push((arr[i]));
+    //     }
+    // }
+
+    // return res;  
+
+    let biggestValue = arr[0];
+
+    let res = arr.reduce((acc, currValue) => {
+        if(currValue >= biggestValue) {
+            biggestValue = currValue;
+            acc.push(currValue);
         }
-    }
+        return acc;
+    }, []);
 
-    return res;  
+    console.log(res);
 }
-extracting([20, 
-    3, 
-    2, 
-    15,
-    6, 
-    1])
+extracting([1, 2, 3, 4])
