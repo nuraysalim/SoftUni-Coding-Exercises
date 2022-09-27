3708b6fbb284fedc87531d90be1e7a0005dc4c8f
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-27 20:47:09 +0300

    01. Calorie Object

---
+++ Advanced JS Module/JS Advanced/3. Objects and Composition/Exercise/01.calorieObject.js
@@ -0,0 +1,10 @@
+function calorieObject(arr) {
+    let obj = {};
+
+    for (let i = 0; i < arr.length; i += 2) {
+        obj[arr[i]] = Number(arr[i + 1]);
+    }
+
+    return obj;
+}
+calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
