e23c09f4daf556dad1a7c0326e590b76f5ffa5f5
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-27 17:44:59 +0300

    05. Assembly Line

--- Advanced JS Module/JS Advanced/3. Objects and Composition/Lab/05.assemblyLine.js
+++ Advanced JS Module/JS Advanced/3. Objects and Composition/Lab/05.assemblyLine.js
@@ -16,7 +16,7 @@ function createAssemblyLine() {
      hasAudio: (obj) => {
         obj.currentTrack = null;
         obj.nowPlaying = () => {
-                console.log(`Now playing '${currentTrack.name}' by ${currentTrack.artist}`);
+                console.log(`Now playing '${obj.currentTrack.name}' by ${obj.currentTrack.artist}`);
         };    
     },
 
@@ -24,9 +24,9 @@ function createAssemblyLine() {
         obj.checkDistance = (distance) => {
             if(distance < 0.1) {
                 console.log("Beep! Beep! Beep!");
-            } else if(distance <= 0.1  && distance < 0.25) {
+            } else if(distance >= 0.1  && distance < 0.25) {
                 console.log("Beep! Beep!");
-            } else if(distance <= 0.25 && distance < 0.5) {
+            } else if(distance >= 0.25 && distance < 0.5) {
                 console.log("Beep!");
             } else {
                 console.log('');
@@ -37,22 +37,34 @@ function createAssemblyLine() {
 }
 }
 
+//Setup:
 const assemblyLine = createAssemblyLine();
 
 const myCar = {
     make: 'Toyota',
     model: 'Avensis'
 };
-// Testing the hasClima decorator function:
-// assemblyLine.hasClima(myCar);
-// console.log(myCar.temp);
-// myCar.tempSettings = 18;
-// myCar.adjustTemp();
-// console.log(myCar.temp);
 
+//Testing the hasClima decorator function:
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+
+
+//Tests the hasAudio decorator function:
 assemblyLine.hasAudio(myCar);
 myCar.currentTrack = {
     name: 'Never Gonna Give You Up',
     artist: 'Rick Astley'
 };
 myCar.nowPlaying();
+
+//Tests the hasParktronic decorator function:
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+
+//All the changes in the object at the end:
+console.table(myCar);
