b4144e1c0493412b33b888407edca2c71cbf7d65
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-27 08:21:08 +0300

    Update 05. Assembly Line - still not finished yet

--- Advanced JS Module/JS Advanced/3. Objects and Composition/Lab/05.assemblyLine.js
+++ Advanced JS Module/JS Advanced/3. Objects and Composition/Lab/05.assemblyLine.js
@@ -1,14 +1,40 @@
 function createAssemblyLine() {
     
-    function hasClima(obj) {
+   return { 
+    hasClima: (obj) => {
         obj.temp = 21;
         obj.tempSettings = 21;
         obj.adjustTemp = () => {
             if(obj.temp < obj.tempSettings) {
-                
-            }
+                obj.temp++;
+            } else if (obj.temp > obj.tempSettings) {
+                obj.temp--;
+            } 
         }
+    },
+
+     hasAudio: (obj) => {
+        obj.currentTrack = null;
+        obj.nowPlaying = () => {
+                console.log(`Now playing '${currentTrack.name}' by ${currentTrack.artist}`);
+        };    
+    },
+
+   hasParktronic: (obj) => {
+        obj.checkDistance = (distance) => {
+            if(distance < 0.1) {
+                console.log("Beep! Beep! Beep!");
+            } else if(distance <= 0.1  && distance < 0.25) {
+                console.log("Beep! Beep!");
+            } else if(distance <= 0.25 && distance < 0.5) {
+                console.log("Beep!");
+            } else {
+                console.log('');
+            }
+        };
+
     }
+}
 }
 
 const assemblyLine = createAssemblyLine();
@@ -17,3 +43,16 @@ const myCar = {
     make: 'Toyota',
     model: 'Avensis'
 };
+// Testing the hasClima decorator function:
+// assemblyLine.hasClima(myCar);
+// console.log(myCar.temp);
+// myCar.tempSettings = 18;
+// myCar.adjustTemp();
+// console.log(myCar.temp);
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+    name: 'Never Gonna Give You Up',
+    artist: 'Rick Astley'
+};
+myCar.nowPlaying();
