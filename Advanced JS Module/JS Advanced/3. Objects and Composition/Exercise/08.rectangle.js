65b52ae09da190be962a211d686f8f77ccf5e926
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-28 21:16:12 +0300

    08. Rectangle

--- Advanced JS Module/JS Advanced/3. Objects and Composition/Exercise/08.rectangle.js
+++ Advanced JS Module/JS Advanced/3. Objects and Composition/Exercise/08.rectangle.js
@@ -2,7 +2,7 @@ function rectangle(width, height, color) {
     return {
         width,
         height,
-        color: color[0].toUpperCase(),
+        color: color[0].toUpperCase() + color.slice(1),
         calcArea: () => {
             return width * height;
         }
