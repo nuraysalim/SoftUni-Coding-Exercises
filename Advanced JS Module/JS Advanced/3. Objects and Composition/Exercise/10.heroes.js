4464b4d16730e661840666b0e4732264809bd793
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-09-29 19:55:40 +0300

    10. Heroes

--- Advanced JS Module/JS Advanced/3. Objects and Composition/Exercise/10.heroes.js
+++ Advanced JS Module/JS Advanced/3. Objects and Composition/Exercise/10.heroes.js
@@ -1,5 +1,37 @@
-function solve(params) {
-    
+function solve() {
+   let mage = (name) => {
+    let state = {
+        name,
+        health: 100,
+        mana: 100
+    };
+    return Object.assign(state, canCast(state));
+   };
+
+   let fighter = (name) => {
+    let state = {
+        name,
+        health: 100,
+        stamina: 100
+    };
+    return Object.assign(state, canFight(state));
+   };
+
+   let canCast = (state) => ({
+    cast: (spell) => {
+    state.mana--;
+    console.log(`${state.name} cast ${spell}`);
+    }
+   });
+
+   let canFight = (state) => ({
+    fight: () => {
+        state.stamina--;
+        console.log(`${state.name} slashes at the foe!`);
+    }
+   });
+
+   return {'mage': mage, 'fighter': fighter}
 }
 let create = solve();
 const scorcher = create.mage("Scorcher");
