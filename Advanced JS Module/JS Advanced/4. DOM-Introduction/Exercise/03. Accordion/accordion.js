24c84d53ccfb256584f195448ab29d559e1f6e9d
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-02 16:49:38 +0300

    03. Accordion

--- Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/03. Accordion/accordion.js
+++ Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonClicked = document.getElementsByClassName('button')[0].textContent;
+    
+    if(buttonClicked === 'More') {
+        document.getElementsByClassName('button')[0].textContent = 'Less'
+        document.getElementById('extra').style.display = 'block';
+    } else {
+        document.getElementsByClassName('button')[0].textContent = 'More';
+        document.getElementById('extra').style.display = 'none';
+    }
 }
