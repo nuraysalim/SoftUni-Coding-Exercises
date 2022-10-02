cfa88d977454344a5f0814d0d201529cead39794
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-02 14:44:20 +0300

    01. Subtraction

--- Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/01. Subtraction/subtract.js
+++ Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNum = document.getElementById('firstNumber').value;
+    let secondNum = document.getElementById('secondNumber').value;
+
+    let substraction = Number(firstNum) - Number(secondNum);
+
+    document.getElementById('result').textContent = substraction;
 }
