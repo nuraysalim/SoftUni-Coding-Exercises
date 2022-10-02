21fcf1fc78762288e18352cfe384f3e673d9ce1b
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-02 15:29:00 +0300

    02. Pascal or Camel Case - to be continued

--- Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value
+  text = text.toLowerCase()
+              .trim()
+              .split(' ');
+  let command = document.getElementById('naming-convention').value;
+  let output = [];
+
+  switch (command) {
+    case 'Camel case': 
+    output.push(text.shift());
+    
+      for (const word of text) {
+        let firstLetter = word.slice(0, 1).toUpperCase();
+        let theRestofWord = word.slice(1);
+        output.push(firstLetter + theRestofWord);
+      };
+      break;
+    case 'Pascal case':
+      for (const word of text) {
+        let firstLetter = word.slice(0, 1).toUpperCase();
+        let theRestofWord = word.slice(1);
+        output.push(firstLetter + theRestofWord);
+      };
+      break;
+    default: output = 'Error!';
+    break;
+  }
+
 }
