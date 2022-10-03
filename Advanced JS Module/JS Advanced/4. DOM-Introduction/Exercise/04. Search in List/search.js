d9afe91e2e09566375511af21f49fd1a51e0d9ef
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-03 19:18:33 +0300

    04. Search in List

--- Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/04. Search in List/search.js
+++ Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/04. Search in List/search.js
@@ -1,16 +1,21 @@
 function search() { 
-   debugger;
    let listOfTowns = Array.from(document.querySelectorAll('ul li'));
    let searchText = document.getElementById('searchText').value;
-   let allTownsArr = [];
-
+   let matchesCounter = 0;
+   
+  
    for (const liRow of listOfTowns) {
       let text = liRow.textContent;
 
       if(text.includes(searchText)){
-
+         liRow.style.textDecoration = 'underline';
+         liRow.style.fontWeight = 'bold';
+         matchesCounter++;
+      } else {
+         liRow.style.textDecoration = 'none';
+         liRow.style.fontWeight = '';
       }
    }
 
-
+   document.getElementById('result').innerText = `${matchesCounter} matches found`;
 }
