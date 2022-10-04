bafa5116a1c3368afc6a93dd421c5f75050495bd
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-04 22:06:42 +0300

    07. Hell's Kitchen - need to finish tomorrow

--- Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/07. Hell's Kitchen/app.js
+++ Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -2,7 +2,29 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+      debugger;
+      let listOfRestaurants = document.querySelector('#inputs textarea').value;
+      listOfRestaurants = JSON.parse(listOfRestaurants);
+      let objOfRest = {};
+
+      for (const restraurantInfo of listOfRestaurants) {
+         let [name, ...employees] = restraurantInfo.split(' - ');
+         let avgSalary = 0;
+         let highestSalary = 0;
+
+         employees = employees[0].split(', ');
+
+         for (const nameAndSalary of employees) {
+            let [nameOfEmpl, salary] = nameAndSalary.split(' ');
+            salary = Number(salary);
+            avgSalary += salary;
+
+            if(highestSalary < salary) {
+               highestSalary = salary;
+            }
+         }
+      }
+
       
    }
 }
