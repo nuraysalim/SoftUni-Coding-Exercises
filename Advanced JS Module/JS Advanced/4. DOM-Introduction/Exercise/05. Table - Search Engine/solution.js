a174066b7195ea0382d22194d05856ab52dec597
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-04 17:11:34 +0300

    05. Table – Search Engine

--- Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/05. Table - Search Engine/solution.js
+++ Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,21 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let inputText = document.getElementById('searchField').value.toLowerCase();
+      let rowsInfo = Array.from(document.querySelectorAll('.container tbody tr'));
 
+      for (const row of rowsInfo) {
+         let text = row.textContent.trim().split('\n');
+
+         for(let info of text) {
+            if(info.toLowerCase().includes(inputText)) {
+               row.classList.add('select');
+               break;
+            } else {
+               row.classList.remove('select');
+            }
+         }
+      }
+      document.getElementById('searchField').value = '';
    }
 }
