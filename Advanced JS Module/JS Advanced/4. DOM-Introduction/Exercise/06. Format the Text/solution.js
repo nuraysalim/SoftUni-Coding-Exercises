ec157ee2d404119b0b58e2076f95dd2969ca0a2f
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-04 20:06:09 +0300

    06. Format the Text

--- Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/06. Format the Text/solution.js
+++ Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO
-}
+    let text = document.getElementById('input').value
+    let arrayText = text.split('.').filter(sentence => sentence.length > 0);
+    let output = document.getElementById('output');
+    output.innerHTML = '';
+
+    for (let i = 0; i < arrayText.length; i += 3) {
+    let paragraphText = [];
+
+      for (let j = 0; j < 3; j++) {
+        if(arrayText[i + j]) {
+          paragraphText.push(arrayText[i + j]);
+        }
+      }
+
+    paragraphText.push('');
+    let resText = paragraphText.join('. ')
+    output.innerHTML += `<p>${resText}</p>`;
+  }
+  }
