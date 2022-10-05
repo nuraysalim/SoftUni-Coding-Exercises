418a918533dd82178280ba7b188e98d23fbabdea
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-05 18:43:32 +0300

    07. Hell's Kitchen - finished finally

--- Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/07. Hell's Kitchen/app.js
+++ Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -2,75 +2,64 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
+      // We take the input and we parse it to an array:
       let listOfRestaurants = document.querySelector('#inputs textarea').value;
       listOfRestaurants = JSON.parse(listOfRestaurants);
+      // I take the referense to the output elements and clear them:
+      let bestRestaurant = document.querySelector('#bestRestaurant p');
+      let salariesOfEmployees = document.querySelector('#workers p');
+      bestRestaurant.textContent = '';
+      salariesOfEmployees.textContent = '';
       let objOfRest = {};
-      let restaurantContentText = document.querySelector('#bestRestaurant p');
-      let employeesContentText = document.querySelector('#workers p');
-      restaurantContentText.textContent = '';
-      employeesContentText.textContent = '';
 
       for (const restraurantInfo of listOfRestaurants) {
          let [name, ...currEmployees] = restraurantInfo.split(' - ');
-         let avgSalary = 0;
+         let sumSalary = 0;
          let highestSalary = 0;
 
          if(!objOfRest.hasOwnProperty(name)) {
             objOfRest[name] = {};
             objOfRest[name].allEmployees = []; 
+            objOfRest[name].avgSalary = 0;
+            objOfRest[name].sumSalary = 0
+            objOfRest[name].bestSalary = 0;
          }
 
          currEmployees = currEmployees[0].split(', ');
 
          for (const nameAndSalary of currEmployees) {
-            let splittedInfo = nameAndSalary.split(' ');
+            let splittedInfo = nameAndSalary && nameAndSalary.split(' ');
             objOfRest[name].allEmployees.push(splittedInfo);
 
             let salary = Number(splittedInfo[1]);
-            avgSalary += salary;
+            sumSalary += salary;
 
             if(highestSalary < salary) {
                highestSalary = salary;
             }
          }
 
-         avgSalary = avgSalary / currEmployees.length;
+         objOfRest[name].sumSalary += sumSalary;
 
-         if(!objOfRest[name].hasOwnProperty('averageSalary')) {
-            objOfRest[name].averageSalary = avgSalary;
-            objOfRest[name].bestSalary = highestSalary;
-            continue;
-         }
-
-         let previousCountEmpl = objOfRest[name].allEmployees.length - currEmployees.length;
-         let oldAvgSalary = objOfRest[name].averageSalary * previousCountEmpl;
-         avgSalary = (avgSalary + oldAvgSalary) / objOfRest[name].allEmployees.lengt;
-
-         objOfRest[name].averageSalary = avgSalary;
+        objOfRest[name].avgSalary = objOfRest[name].sumSalary / objOfRest[name].allEmployees.length;
          objOfRest[name].bestSalary = objOfRest[name].bestSalary < highestSalary ? highestSalary : objOfRest[name].bestSalary;
       }
 
+      let sortRes = Object.entries(objOfRest).sort(([nameA, infoA], [nameB, infoB]) => {
+       return  infoB.avgSalary - infoA.avgSalary;
+      })[0];
 
-      let bestRestraurant;
-      let bestAvgSalary = 0;
-
-      for (const name in objOfRest) {
-        let currAvgSalary = objOfRest[name].averageSalary;
-
-        if(currAvgSalary > bestAvgSalary) {
-         bestAvgSalary = currAvgSalary;
-         bestRestraurant = name;
-        };
-      };
-
-      restaurantContentText.textContent = `Name: ${bestRestraurant} Average Salary: ${bestAvgSalary.toFixed(2)} Best Salary: ${objOfRest[bestRestraurant].bestSalary.toFixed(2)}`;
-      let sortedBySalary = objOfRest[bestRestraurant].allEmployees.sort(([nameA, salaryA], [nameB, salaryB]) => {
+      bestRestaurant.textContent = `Name: ${sortRes[0]} Average Salary: ${sortRes[1].avgSalary.toFixed(2)} Best Salary: ${sortRes[1].bestSalary.toFixed(2)}`;
+      
+      let employeesSorted = objOfRest[sortRes[0]].allEmployees.sort(([nameA, salaryA], [nameB, salaryB]) => {
          return salaryB - salaryA;
       });
 
-      for (const [nameOfEmployee, salary] of sortedBySalary) {
-         employeesContentText.textContent +=
+      let buff = ''
+      for (const [nameOfEmployee, salary] of employeesSorted) {
+         buff +=
          `Name: ${nameOfEmployee} With Salary: ${salary} `;
       };
+      salariesOfEmployees.textContent += buff;
    }
 }
