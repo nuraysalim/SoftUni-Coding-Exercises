287843291967f55162a3d011c49e746508c0b92e
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-05 20:12:31 +0300

    08. Generate Report

--- Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/08. Generate Report/report.js
+++ Advanced JS Module/JS Advanced/4. DOM-Introduction/Exercise/08. Generate Report/report.js
@@ -1,3 +1,19 @@
 function generateReport() {
-    //TODO
+    let headers = document.querySelectorAll('thead th input');
+    let tableRow = document.querySelectorAll('tbody tr')
+    let res = [];
+
+   for (let i = 0; i < tableRow.length; i++) {
+    let tableData = tableRow[i].cells;
+    let tempData = {};
+     
+    for(let j = 0; j < tableData.length; j++) {
+        if(headers[j].checked) {
+            tempData[headers[j].name] = tableData[j].textContent;
+        }
+    }
+    res.push(tempData);
+    }
+
+    document.getElementById('output').textContent = JSON.stringify(res);
 }
