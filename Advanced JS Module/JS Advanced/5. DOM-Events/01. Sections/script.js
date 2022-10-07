9b9a25c6eae4d5b68d287130a81a41dd18dbc265
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-07 06:26:45 +0300

    01. Sections

--- Advanced JS Module/JS Advanced/5. DOM-Events/01. Sections/script.js
+++ Advanced JS Module/JS Advanced/5. DOM-Events/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   let parentDiv = document.getElementById('content');
+
+  for (const word of words) {
+   let newDiv = document.createElement('div');
+   let p = document.createElement('p'); 
+
+   p.textContent = word;
+   p.style.display = 'none';
+
+   newDiv.appendChild(p);
+   newDiv.addEventListener('click', () => {
+      p.style.display = '';
+   })
+   parentDiv.appendChild(newDiv);
+ }
 }
