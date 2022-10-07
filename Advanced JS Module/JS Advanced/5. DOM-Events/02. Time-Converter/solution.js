16c444eb91a2d6cada12bc617d475ee0f48fb6bb
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-07 19:15:47 +0300

    02. Time-Converter

--- Advanced JS Module/JS Advanced/5. DOM-Events/02. Time-Converter/solution.js
+++ Advanced JS Module/JS Advanced/5. DOM-Events/02. Time-Converter/solution.js
@@ -1,10 +1,38 @@
 function attachEventsListeners() {
-    let parent = document.getElementsByTagName('main');
+    let buttons = document.querySelectorAll('input[type=button]');
+    
+    for (const button of buttons) {
+        button.addEventListener('click', convert);
+    }
+    
+    function convert(e) {
+         let input = Number(e.target.parentNode.querySelector('input[type=text]').value);
+        let idOfBtn = e.target.id;
+        let toDays;
 
-    parent.addEventListener('click', convert);
-
-    function convert() {
-        let input;
+         switch (idOfBtn) {
+             case 'daysBtn': toDays = input;
+                 break;
+             case 'hoursBtn':
+               toDays = input / 24;
+                 break;
+             case 'minutesBtn':
+                toDays = input / 1440
+                 break;
+             case 'secondsBtn':
+                toDays = input / 86400;
+                 break;
+    }
+    populate(toDays);
     }
 
+    function populate(value) {
+        let inputs = Array.from(document.querySelectorAll('input[type=text]'));
+        inputs.shift().value = value;
+       let currentValue = value * 24;
+        for (const input of inputs) {
+            input.value = currentValue;
+            currentValue *= 60;
+        }
+    }
 }
