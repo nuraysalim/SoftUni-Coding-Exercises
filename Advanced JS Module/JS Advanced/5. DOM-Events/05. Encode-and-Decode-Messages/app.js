541c95723108c7c09b92758b5415f2319a2dbc6e
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-08 18:59:49 +0300

    05. Encode and Decode Messages 1 ​

--- Advanced JS Module/JS Advanced/5. DOM-Events/05. Encode-and-Decode-Messages/app.js
+++ Advanced JS Module/JS Advanced/5. DOM-Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,48 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let paragraphs = document.getElementsByTagName('p');
+    let mainDivChildren = document.getElementById('main').children;
+    let text;
+
+    for (const p of paragraphs) {
+        let btn = p.parentElement.getElementsByTagName('button')[0];
+
+        if(p.textContent === 'Message') {
+           btn.addEventListener('click', encode);
+        } else {
+           btn.addEventListener('click', decode);
+        }
+    }
+
+    function encode() {
+        let message = mainDivChildren[0].getElementsByTagName('textarea')[0];
+        text = message.value;
+        
+        let encodedText = '';
+
+        for (const character of text) {
+            let change = character.charCodeAt();
+            change++;
+            encodedText += String.fromCharCode(change);
+        }
+
+        message.value = '';
+
+        let encodedTextArea = mainDivChildren[1].getElementsByTagName('textarea')[0];
+        encodedTextArea.value = encodedText; 
+    }
+
+    function decode() {
+        let encodedMessage = mainDivChildren[1].getElementsByTagName('textarea')[0];
+        text = encodedMessage.value;
+
+        let decodedText = '';
+
+        for (const character of text) {
+            let letter = character.charCodeAt();
+            letter--;
+            decodedText += String.fromCharCode(letter);
+        }
+
+        encodedMessage.value = decodedText;
+    }
 }
