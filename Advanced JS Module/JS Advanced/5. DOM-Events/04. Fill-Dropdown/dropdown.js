a43ba0e0321a1e14d0ab72608af123990350846f
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-08 18:05:19 +0300

    04. Fill Dropdown

--- Advanced JS Module/JS Advanced/5. DOM-Events/04. Fill-Dropdown/dropdown.js
+++ Advanced JS Module/JS Advanced/5. DOM-Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemText = document.getElementById('newItemText');
+    let itemValue = document.getElementById('newItemValue');
+    
+    let optionEl = document.createElement('option');
+    optionEl.textContent = itemText.value;
+    optionEl.value = itemValue.value;
+
+    let selectMenu = document.getElementById('menu');
+    selectMenu.appendChild(optionEl);
+
+    itemText.value = '';
+    itemValue.value = '';
 }
