4b312d0991516345a4dee1237d08ec4bc0c42ddd
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-08 17:48:17 +0300

    03. Locked Profile

--- Advanced JS Module/JS Advanced/5. DOM-Events/03. Locked-Profile/app.js
+++ Advanced JS Module/JS Advanced/5. DOM-Events/03. Locked-Profile/app.js
@@ -1,3 +1,34 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = document.getElementsByTagName('button');
+    
+    for (const button of buttons) {
+        button.addEventListener('click', showOrHide);
+    }
+
+    function showOrHide(e) {
+        let lockOrUnlocked = e.target.parentNode.querySelectorAll('input[type=radio]');
+
+        for (const btn of lockOrUnlocked) {
+            if(!btn.checked) {
+                continue;
+            }
+            let checkedBtn = btn.value;
+
+            if(e.target.textContent === 'Show more') {
+
+                if(checkedBtn === 'unlock') {
+                    let hiddenInfo = e.target.parentNode.getElementsByTagName('div');
+                    hiddenInfo[0].style.display = 'block';
+                    e.target.textContent = 'Hide it';
+                }   
+            } else {
+
+                if(checkedBtn === 'unlock') {
+                    let hiddenInfo = e.target.parentNode.getElementsByTagName('div');
+                    hiddenInfo[0].style.display = 'none';
+                    e.target.textContent = 'Show more';
+                }
+            }
+        }
+    }
 }
