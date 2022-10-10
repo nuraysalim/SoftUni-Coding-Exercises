f6d6d8a778dc761e66d5cf3957383edd98c14a93
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-10 15:13:32 +0300

    03. Fibonacci

--- Advanced JS Module/JS Advanced/6. Advanced Functions/03.fibonacci.js
+++ Advanced JS Module/JS Advanced/6. Advanced Functions/03.fibonacci.js
@@ -1,3 +1,23 @@
-function fibonacci(params) {
-    
+function getFibonator() {
+    let arr = [0, 1];
+    let firstOnePrinted = false;
+    let res;
+    return function fibonacci() {
+        if(firstOnePrinted) {
+             res = arr[arr.length - 2] + arr[arr.length - 1];;
+            arr.push(res);
+        } else {
+            res = 1;
+            firstOnePrinted = true;
+        }
+        return res;
+    }
 }
+let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
