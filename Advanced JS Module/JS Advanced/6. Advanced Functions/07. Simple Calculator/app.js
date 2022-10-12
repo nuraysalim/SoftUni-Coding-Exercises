e64786f4e3285249e2eee4b657a18fa1702f212b
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-12 20:04:29 +0300

    ....

--- Advanced JS Module/JS Advanced/6. Advanced Functions/07. Simple Calculator/app.js
+++ Advanced JS Module/JS Advanced/6. Advanced Functions/07. Simple Calculator/app.js
@@ -1,7 +1,29 @@
 function calculator() {
-    // TODO:
-}
+    let selector1;
+    let selector2;
+    let resultSelector;
+    let action = {
+        init: (firstSelector, secondSelector, result) => {
+            selector1 = document.querySelector(firstSelector);
+            selector2 = document.querySelector(secondSelector);
+            resultSelector = document.querySelector(result);
+        },
+        add: () => {
+            let firstNum = Number(selector1.value);
+            let secondNum = Number(selector2.value);
+            let sum = firstNum + secondNum;
+            resultSelector.value = sum;
+        },
+        subtract: () => {
+            let firstNum = Number(selector1.value);
+            let secondNum = Number(selector2.value);
+            let sum = firstNum - secondNum;
+            resultSelector.value = sum;
+        }
+    }
 
+    return action;
+}
 
 
 
