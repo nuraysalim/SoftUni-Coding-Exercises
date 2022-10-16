aec4711989dd912f4351fa92a172630231b34461
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-16 11:13:52 +0300

    02. Even Or Odd

--- Advanced JS Module/JS Advanced/7. Unit Testing and Error Handling/Tests/02.evenOrOdd.test.js
+++ Advanced JS Module/JS Advanced/7. Unit Testing and Error Handling/Tests/02.evenOrOdd.test.js
@@ -1,32 +1,32 @@
-let expect  = require('chai').assert;
+let {expect}  = require('chai');
 let {isOddOrEven} = require("../02.evenOrOdd");
 
 describe('Testing functionality', ()=> {
     describe('Test isOddOrEven with incorrect values', () => {
         it('Result is undefined with an array argument', () => {
-            expect.equal(isOddOrEven([]), undefined);
+            expect(isOddOrEven([])).to.equal(undefined);
         });
     
         it('Result is undefined with an object argument', () => {
-            expect.equal(isOddOrEven({}), undefined);
+            expect(isOddOrEven({})).to.equal(undefined);
         });
     
         it('Result is undefined with parsed number as an argument', () => {
-            expect.equal(isOddOrEven(1), undefined);
+            expect(isOddOrEven(1)).to.equal(undefined);
         });
     
         it('Result is undefined with a boolean argument', () => {
-            expect.equal(isOddOrEven(true), undefined);
+            expect(isOddOrEven(false)).to.equal(undefined);
         });
     });
 
     describe('Test isOddOrEven with correct values', ()=> {
         it('Result is even with "cats"', ()=> {
-            expect.equal(isOddOrEven('cats'), 'even');
+            expect(isOddOrEven('cats')).to.equal('even');
         });
 
         it('Result is odd with "cat"', ()=>{
-            expect.equal(isOddOrEven('cat'), 'odd');
+            expect(isOddOrEven('cat')).to.equal('odd');
         })
     });
 });
