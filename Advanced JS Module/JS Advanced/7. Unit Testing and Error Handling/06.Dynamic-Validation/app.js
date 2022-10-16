0f33e110817c1109f60034b26a08211126cf1f30
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-16 22:04:10 +0300

    06. Dynamic Validation

--- Advanced JS Module/JS Advanced/7. Unit Testing and Error Handling/06.Dynamic-Validation/app.js
+++ Advanced JS Module/JS Advanced/7. Unit Testing and Error Handling/06.Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    // TODO
+   let email = document.getElementById('email');
+   email.addEventListener('change', dynamicValidation);
+
+
+   function dynamicValidation() {
+        let pattern = /\b[a-z]+@[a-z]+.[a-z]+/g;
+        let emailInput = email.value;
+
+        if(!emailInput.match(pattern)) {
+            email.classList.add('error');
+        } else {
+            email.classList.remove('error');
+        }
+   }
 }
