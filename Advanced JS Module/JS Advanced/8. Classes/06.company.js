b2aa0f0d9a3c1a2a98a9b8586d78a2424cfc8b92
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-10-18 15:07:04 +0300

    06. Company

--- Advanced JS Module/JS Advanced/8. Classes/06.company.js
+++ Advanced JS Module/JS Advanced/8. Classes/06.company.js
@@ -8,20 +8,46 @@ class Company {
             throw new Error ("Invalid input!");
         }
 
-        if(!departments[department]) {
+        if(!this.departments[department]) {
             this.departments[department] = {
                 avgSalary: 0,
                 sumSalary: 0,
                 employees: []
             }
         }
+        
         this.departments[department].employees.push({name, salary, position});
+        this._updateDepartment(this.departments[department], salary);
 
-
+        return `New employee is hired. Name: ${name}. Position: ${position}`;
     }
 
-    updateDepartment () {
-        
+    _updateDepartment (department, employeeSalary) {
+        department.sumSalary += Number(employeeSalary);
+        department.avgSalary = department.sumSalary / department.employees.length; 
+    }
+
+    bestDepartment () {
+       let bestDept = Object.entries(this.departments).sort(([departmentOne, deptInfoOne], [departmentTwo, deptInfoTwo]) =>{
+            return deptInfoOne - deptInfoTwo;
+        })[0];
+        let sortedEmployees = bestDept[1].employees.sort((objOne, objTwo) =>{
+         return objTwo.salary - objOne.salary || (objOne.name).localeCompare(objTwo.name);
+        });
+
+        let buff = '';
+        let counter = 0;
+        let numOfEmployees = sortedEmployees.length - 1;
+        for (const obj of sortedEmployees) {
+            if(numOfEmployees === counter) {
+                buff += `${obj.name} ${obj.salary} ${obj.position}`;
+                break;
+            } 
+            buff += `${obj.name} ${obj.salary} ${obj.position}\n`;
+            counter++;
+        };
+
+        return `Best Department is: ${bestDept[0]}\nAverage salary: ${bestDept[1].avgSalary.toFixed(2)}\n${buff}`
     }
 }
 let c = new Company();
