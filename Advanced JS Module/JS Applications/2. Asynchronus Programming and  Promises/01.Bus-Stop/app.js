2c4b295fe8de6849a6f5d2145d135a4b06f8c93c
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-11-01 09:24:09 +0200

    01. Bus-Stop

--- Advanced JS Module/JS Applications/2. Asynchronus Programming and  Promises/01.Bus-Stop/app.js
+++ Advanced JS Module/JS Applications/2. Asynchronus Programming and  Promises/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const infoElement = document.getElementById('stopId');
+    const stopId = infoElement.value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+    const stopName = document.getElementById('stopName');
+    const ulElement = document.getElementById('buses');
+
+    ulElement.innerHTML = "";
+    stopId.value = "";
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        stopName.textContent = data.name;
+        Object.entries(data.buses).forEach(([bus, arivalTime]) => {
+            const li = document.createElement('li');
+            li.textContent = `Bus ${bus} arrives in ${arivalTime} minutes`;
+            ulElement.appendChild(li);
+        });
+    } catch (e) {
+        stopName.textContent = "Error";
+    };
 }
