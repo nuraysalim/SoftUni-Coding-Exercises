a4276744b58a267bbc2acf8284ade43fe9025a37
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-11-01 11:16:35 +0200

    Not finished 03. Forecaster

--- Advanced JS Module/JS Applications/2. Asynchronus Programming and  Promises/03.Forecaster/app.js
+++ Advanced JS Module/JS Applications/2. Asynchronus Programming and  Promises/03.Forecaster/app.js
@@ -1,5 +1,31 @@
 function attachEvents() {
-    console.log("TODO...");
+    document.getElementById('submit').addEventListener('click', getWeather);
+    
+    
+async function getWeather() {
+    const url = `http://localhost:3030/jsonstore/forecaster/locations`;
+    let townName = document.getElementById('location').value;
+
+    const response = await fetch(url);
+    let data = await response.json();
+
+    let info = data.find(x => x.name === townName);
+
+    createForcast(info.code);
+    }
+
+    async function createForcast(code) {
+        const urlToday = `http://localhost:3030/jsonstore/forecaster/today/${code}`;
+        const urlThreeDays = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`;
+
+
+        const responseToday = await fetch(urlToday);
+        const dataToday = await responseToday.json();
+        const responseThreeDays = await fetch(urlThreeDays);
+        const dataThreeDays = await responseThreeDays.json();
+        
+
+    }
 }
 
 attachEvents();
