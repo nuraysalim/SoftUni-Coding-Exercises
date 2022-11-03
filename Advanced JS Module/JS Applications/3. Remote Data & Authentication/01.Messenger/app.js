ce7cb8b31ef904b688686b14108c4bff2f9f8209
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-11-03 20:33:35 +0200

    01. Messenger 100/100

--- Advanced JS Module/JS Applications/3. Remote Data & Authentication/01.Messenger/app.js
+++ Advanced JS Module/JS Applications/3. Remote Data & Authentication/01.Messenger/app.js
@@ -1,5 +1,50 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('refresh').addEventListener('click', getAllMsg);
+    document.getElementById('submit').addEventListener('click', onSendMsg);
 }
 
+function renderMsg(data) {
+    const textArea = document.getElementById('messages');
+   const content = Object.values(data).map(entry => `${entry.author}: ${entry.content}`).join('\n');
+    textArea.textContent = content;
+   debugger
+}
+
+function onSendMsg() {
+    const author = document.querySelector('input[name="author"]');
+    const content = document.querySelector('input[name="content"]');
+
+    const body = {
+        author: author.value,
+        content: content.value
+    };
+
+    author.value = "";
+    content.value = "";
+
+    createMsg(body)
+}
+async function getAllMsg() {
+    const url = `http://localhost:3030/jsonstore/messenger`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    renderMsg(data)
+}
+
+async function createMsg(body) {
+    const url = `http://localhost:3030/jsonstore/messenger`;
+
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    const data = await res.json();
+    
+    getAllMsg()
+}
 attachEvents();
