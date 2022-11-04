ed9a19f6b36f06a7b44aaa54712e79720e7bc897
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-11-04 15:59:44 +0200

    02. Bus-Schedule 100/100

--- Advanced JS Module/JS Applications/2. Asynchronus Programming and  Promises/02.Bus-Schedule/app.js
+++ Advanced JS Module/JS Applications/2. Asynchronus Programming and  Promises/02.Bus-Schedule/app.js
@@ -1,11 +1,41 @@
 function solve() {
+    let id = 'depot';
+    let stopData;
+    const infoBox = document.querySelector(".info");
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+   async function depart() {
+    
+    const url = `http://localhost:3030/jsonstore/bus/schedule/${id}`;
+
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        id = data.next;
+        stopData = data;
+    
+        updateInfo(stopData);
+    } catch {
+        infoBox.textContent = "Error";
+        departBtn.disabled = true;
+        arriveBtn.disabled = true;
+    }
     }
 
+    function updateInfo(stopData) {
+        infoBox.textContent = `Next stop ${stopData.name}`;
+
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+
+    }
+    
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.textContent = `Arriving at ${stopData.name}`;
+
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
