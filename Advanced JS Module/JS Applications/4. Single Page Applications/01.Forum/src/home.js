b57687e8f654bceb49068ba23228240133da5a4e
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-11-12 15:52:59 +0200

    Made progress 01. Forum

--- Advanced JS Module/JS Applications/4. Single Page Applications/01.Forum/src/home.js
+++ Advanced JS Module/JS Applications/4. Single Page Applications/01.Forum/src/home.js
@@ -5,11 +5,43 @@ const main = document.getElementsByTagName('main')[0];
 const form = document.querySelector('#home-view form');
 form.addEventListener('submit', onSubmit); 
 section.remove();
+const url = `http://localhost:3030/jsonstore/collections/myboard/posts`;
 
+export async function showHome() {
+    const topiContainer = section.querySelector('.topic-title');
+    const posts = await loadPost();
+    const content = Object.values(posts).map(topicTemplate);
 
-export function showHome() {
+    topiContainer.replaceChildren(...content);
+
+debugger;
     main.replaceChildren(section);   
     
+}
+
+function topicTemplate(data) {
+    const container = document.createElement('div');
+    container.classList.add('topic-container');
+    container.innerHTML = `
+        <div class="topic-name-wrapper"">
+            <div class="topic-name">
+                <a href="#" class="normal" id="${data._id}">
+                    <h2>${data.topicName}</h2>
+                </a>
+                <div class="columns">
+                    <div>
+                        <p>Date: <time>${data.date}</time></p>
+                        <div class="nick-name">
+                            <p>Username: <span>${data.username}</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>           
+    `;
+
+    container.querySelector('a').addEventListener('click', showDetails);
+    return container;
 }
 
 function onSubmit(e) {
@@ -22,7 +54,7 @@ function onSubmit(e) {
     const formData = new FormData(form);
     const {topicName, username, postText} = Object.fromEntries(formData);
 
-    createPost({topicName, username, postText});
+    createPost({topicName, username, postText, date: new Date()});
     clearForm();
     debugger
 };
@@ -32,7 +64,6 @@ function clearForm() {
 }
 
 async function createPost(body) {
-    const url = `http://localhost:3030/jsonstore/collections/myboard/posts`;
 
     const response = await fetch(url, {
         method: "POST",
@@ -40,6 +71,12 @@ async function createPost(body) {
         body: JSON.stringify(body)
     });
 
+    const data = await response.json();
+    return data;
+}
+
+async function loadPost () {
+    const response = await fetch(url);
     const data = await response.json();
     return data;
 }
