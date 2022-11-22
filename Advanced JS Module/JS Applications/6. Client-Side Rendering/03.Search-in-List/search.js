7a0e668bc3dd6b8dc9e59c41b063df95d9b524f9
Nuray Salim <106107365+nuraysalim@users.noreply.github.com>
2022-11-22 21:30:55 +0200

    03. Search in List

--- Advanced JS Module/JS Applications/6. Client-Side Rendering/03.Search-in-List/search.js
+++ Advanced JS Module/JS Applications/6. Client-Side Rendering/03.Search-in-List/search.js
@@ -1,3 +1,33 @@
-function search() {
-   // TODO
+import {html, render} from './node_modules/lit-html/lit-html.js';
+import {towns} from './towns.js'
+
+const townsRoot = document.getElementById("towns");
+const resultRoot = document.getElementById("result");
+document.querySelector('button').addEventListener('click', search)
+
+update()
+function searchTemplate(townsName) {
+   const ul = html`
+      <ul>
+         ${townsName.map(townName => createLiTemplate(townName))}
+      </ul>
+   `;
+   return ul;
 }
+
+function createLiTemplate(town) {
+    return html`
+    <li>${town}</li>
+    `;
+};
+
+function update() {
+   const ul = searchTemplate(towns);
+   render(ul, townsRoot);
+}
+
+function search(e) {
+   //to be continued
+
+}
+
